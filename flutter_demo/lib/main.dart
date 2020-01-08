e8b651374048e39267c154ba64502d03b076a111
王喜 <wangxi@njbandou.onaliyun.com>
2020-01-08 17:36:00 +0800

    提交代码

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'contacts_demo.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var stack = new Stack(
+    alignment: const Alignment(0.1, 0.7),
+    children: <Widget>[
+      new CircleAvatar(
+        backgroundImage: new AssetImage('images/jywy.jpeg'),
+        radius: 100.0,
+      ),
+      new Container(
+        decoration: new BoxDecoration(
+          color: Colors.black45,
+        ),
+        child: new Text(
+          'Wang Mia',
+          style: new TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      )
+    ],
+  );
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,9 +39,7 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
         ),
-        body: new Center(
-          child: new Text('Hello World'),
-        ),
+        body: stack,
       ),
     );
   }
